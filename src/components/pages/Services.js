11013c1fef2a21dac17e777cda005584a093f11c
Nguyen-van-minh <2001minhnguyen@gmail.com>
2021-12-23 21:07:55 +0700

    responsive

---
+++ src/components/pages/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+
+
+export default function Services() {
+  return (
+    <>
+    <h1 style={{backgroundImage: "url('images/img-1.jpg')"}} className='services'>SERVICES</h1>
+   
+    {/* <img  src='images/img-9.jpg'  alt="Image"/> */}
+   
+    </>
+  );
+}
